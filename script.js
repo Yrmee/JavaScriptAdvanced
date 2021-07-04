6f24cb2553ee04b6a510eff355237629d030d313
Yrmee <80338690+Yrmee@users.noreply.github.com>
2021-07-04 14:34:23 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
-// zet de functie om naar een arrow functie
-const ikRockArrowFunctions = function () {
-    console.log("Joe, ik rock de arrow functions!");
-  };
+console.log("Opdracht: DOM Manipulatie: dynamisch elementen toevoegen aan de DOM.");
+
+// Deel 1 - Elementen toevoegen aan de DOM
+
+const bigFiveButton = Object.values(
+    document.getElementsByClassName("big-five-button")
+  );
+  const spottedMenu = document.getElementById("spotted-animals-list");
+  const removeItemButton = document.getElementById("remove-first-item-button");
+  const removeAllButton = document.getElementById("remove-all-button");
   
-    ikRockArrowFunctions()
-  // Deel A: Bekijk de bovenstaande functie. Bouw deze functie om tot een arrow function.
-  const ikRockArrowFunctionsA = () => {
-    console.log("Joe, ik rock de arrow functions!");
-  };
-  
-  ikRockArrowFunctionsA()
+  bigFiveButton.forEach((btn) => {
+    btn.addEventListener("click", function () {
+      let animal = btn.innerHTML;
+      let newElement = document.createElement("li");
+      newElement.classList.add("spotted-animals-list-item");
+      newElement.innerHTML = animal;
+      spottedMenu.appendChild(newElement);
+    });
+  });
 
-// Deel B: schrijf de onderstaande functie om naar een arrow functie op 1 regel.
-const fivePlusSeven = function () {
-    return 5 + 7
-  };
-  
-  fivePlusSeven();
-  // Mijn oplossing:
-  const fivePlusSevenB = () => 5 + 7;
-  fivePlusSevenB();
+// Deel 2 - 1 element verwijderen uit de DOM.
 
-/* Deel C: Schrijf een arrow function, op 1 regel (dus met een impliciete return statement), die de params a en b bij-elkaar optelt. 
-Noem de functie myFunction. 
-*/
-const myFunction = (a, b) => a + b;
-  myFunction();
+removeItemButton.addEventListener("click", function () {
+    let firstAnimal = spottedMenu.getElementsByClassName(
+      "spotted-animals-list-item"
+    )[0];
+    spottedMenu.removeChild(firstAnimal);
+  });
 
-// Deel D: Schrijf een arrow function op 1 regel, die de parameter a behoudt, en altijd a plus 5 returned. Noem de functie addFive.
-const addFive = number => number + 5;
-  addFive();
+// Deel 3 Meerdere element verwijderen uit de DOM.
 
-/* Deel E: Schrijf een arrow function met de naam createObject met een implicit return statement (dus op 1 regel) 
-die een simpel object returned: {greeting: "hoi"}
-*/
-createObject => ({greeting: "hoi"}) 
-
+removeAllButton.addEventListener("click", function () {
+    let allAnimals = spottedMenu.getElementsByClassName(
+      "spotted-animals-list-item"
+    );
+    let allAnimalsArray = [].slice.call(allAnimals);
+    allAnimalsArray.forEach((all) => all.remove());
+  });
